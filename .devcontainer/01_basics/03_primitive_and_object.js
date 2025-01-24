10a522b6078db30e11eec820fbbd5c57bdd134ca
Himanshu4788 <bishthimanshu283@gmail.com>
2025-01-24 05:40:09 +0000

    container config done

---
+++ .devcontainer/01_basics/03_primitive_and_object.js
@@ -0,0 +1,29 @@
+
+/*
+there are 7 primitive data type
+NULL  ,  number , string , symbol, bigint, boolean , undefined..
+*/
+
+let a=null;
+let b=2323;
+let c=true;
+let d= BigInt("567")
+let e="himu"
+let f=Symbol("i m  a himu")
+let g //undefines
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof d); // kaisa data type hai ye
+
+
+
+//non primitve data types - obect . 
+// key-value  pair bhi bolte hai.
+const item = {
+    "himu":true,
+    "anmol":false,
+    "nikita":46,
+
+}
+
+console.log(item["nikita"]); //output:46
--- .devcontainer/01_basics/variable_02.js
+++
@@ -1,6 +0,0 @@
-"use strict";//trest all js as newly version 
-
-// alert("hello") gave error coz we are using nodejs ,not browser .  
-
-
-
