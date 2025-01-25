c24a6d8b80e449781c0e9a1d7e7eff6d7de23fd8
Himanshu4788 <bishthimanshu283@gmail.com>
2025-01-25 03:33:24 +0000

    container config done

---
+++ .devcontainer/01_basics/Chapter1/01_lec.js
@@ -0,0 +1,5 @@
+//HTML :- what are the structure of our content.
+//CSS :- how our content is looking ..
+//JS :- how our content  behave/function/action on user action ..
+
+console.log("hello world")
---
+++ .devcontainer/01_basics/Chapter1/04_pratice_set.js
@@ -0,0 +1,40 @@
+
+//create a variable of string and add a number into it.
+let str="hello world" +2
+let nums=8+"hello"
+
+console.log(str); //output hello world2
+console.log(nums); //output hello world2
+
+
+
+//use typeof operation in above datatype.
+console.log(typeof(str));//string
+console.log(typeof(nums));//string
+
+
+//create a const and let  object and in future can it hold a number..
+const a={
+    name:"himu",
+    rollno:75,
+    class:"h1"
+}
+// a=23; // not we can change it for const. gave error
+
+
+let b={
+    naam:"himu",
+    rollno:"75",
+}
+b=95 ; // yes we can change it.
+
+
+
+// can we update anoter key-value and change the  key-value info of const in above question.
+a['city']="dhampur"; // yes we can
+console.log(a);
+
+a['name'] ="lalu" //yes, we can
+console.log(a);
+
+
