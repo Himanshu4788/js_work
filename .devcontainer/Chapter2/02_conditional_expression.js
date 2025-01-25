930bcb11ca6133d1858165385236b48003dd2526
Himanshu4788 <bishthimanshu283@gmail.com>
2025-01-25 04:50:27 +0000

    container congif done

---
+++ .devcontainer/Chapter2/02_conditional_expression.js
@@ -0,0 +1,16 @@
+
+/*
+prompt  =  ye ek input manga hai aur as a string leta hai 
+alert  =    ye alert deta hai
+
+ye isme nhi chalega , iske liye sheedha site mai jao inspect kro and console pe jake likho tb chalega
+*/
+
+
+
+/*
+ 
+conditonal statement same hi hai
+
+
+*/
---
+++ .devcontainer/Chapter2/03_pratice_set.js
@@ -0,0 +1,34 @@
+
+//1.Check if age lies between 10 and 20 using logical operators
+
+
+//2.Demonstrate the use of switch-case statements
+const day = 3; // Replace with any number (1-7) to represent days of the week
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+}
+
+//3.Check if a number is divisible by 2 and 3
+//4.Print "You can drive" or "You cannot drive" based on age
