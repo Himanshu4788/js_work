8f05e197a19c223021a21110d04b18b529287a72
Himanshu4788 <bishthimanshu283@gmail.com>
2025-01-25 04:50:52 +0000

    container config done

--- .devcontainer/01_basics/01_lec.js
+++
@@ -1,5 +0,0 @@
-//HTML :- what are the structure of our content.
-//CSS :- how our content is looking ..
-//JS :- how our content  behave/function/action on user action ..
-
-console.log("hello world")
