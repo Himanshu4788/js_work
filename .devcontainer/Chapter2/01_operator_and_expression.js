a752583d6c633e707cda635390739867b95f51f3
Himanshu4788 <bishthimanshu283@gmail.com>
2025-01-25 04:29:11 +0000

    container config done.

---
+++ .devcontainer/Chapter2/01_operator_and_expression.js
@@ -0,0 +1,71 @@
+
+/*
+ every value which  is written is an expression .
+ fragment of code that produce a value is an expression.
+ ex:- 33,4444.   
+ */
+
+
+//1.arthimatic operator. 
+let a=45
+let b=8
+
+//same wle
+ console.log("a + b =",a+b);
+ console.log("a - b =",a-b);
+ console.log("a * b =",a*b);
+ console.log("a / b =",a/b);
+ console.log("a % b =",a%b);
+ 
+ //exponintial operator
+ console.log("a ** b =",b**b); //b^b
+ console.log("a++ =",a++); 
+ console.log("a-- =",a--); 
+ 
+ 
+ /*
+
+  2.assignment operator.
+  += ,-= , *= , /=, **= , %= 
+  
+  */ 
+
+
+ /*
+
+  3.comparision operator.
+   ==,<= ,>= ,!= , < , > , ?
+  
+  */ 
+
+
+  /*
+  4.logical operator.
+  */ 
+
+  console.log(a&&b); //logical and
+  console.log(a||b); //logical or
+  console.log(!b); //logical not
+
+  
+ /* 
+ 5.bitwise operator.
+ */
+
+  console.log(5 & 0); //0
+  console.log(5 | 3);//7
+  console.log(5 ^ 3);//6
+  console.log(~5);
+  // 5:   0000 0101
+  // ~5:  1111 1010(-6)
+  console.log(5 << 1); 
+  // 5:   0000 0101
+  // <<1: 0000 1010 (10) 
+  console.log(5 >> 1);//2
+  
+  
+   
+
+
+
+ 
